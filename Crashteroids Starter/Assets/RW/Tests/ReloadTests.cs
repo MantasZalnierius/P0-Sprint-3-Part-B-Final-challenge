40819df632e581fe8a635e448d4e3766e03297b8
Ben Millar <c00236772@itcarlow.ie>
2021-10-11 13:16:05 +0100

    [RWMP21-66] Added empty ReloadTest script

---
+++ Crashteroids Starter/Assets/RW/Tests/ReloadTests.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class LaserTest
+{
+    private Game game;
+
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject =
+          MonoBehaviour.Instantiate(
+            Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(game.gameObject);
+    }
+}
