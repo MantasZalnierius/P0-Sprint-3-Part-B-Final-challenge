77ce0ada6abc3732fd43d838451cb24cce25f2d9
AllexisAlvarico <C00242855@itcarlow.ie>
2021-10-11 16:18:22 +0100

    testing working

--- Crashteroids Starter/Assets/RW/Scripts/Spawner.cs
+++ Crashteroids Starter/Assets/RW/Scripts/Spawner.cs
@@ -140,5 +140,42 @@ public class Spawner : MonoBehaviour
         }
 
         shrapnelPieces.Clear();
+        
+    } 
+    public GameObject MaxAsteroid()
+        {
+        int random = Random.Range(1, 5);
+        GameObject asteroid;
+        switch (random)
+        {
+            case 1:
+                asteroid = Instantiate(asteroid1);
+                break;
+            case 2:
+                asteroid = Instantiate(asteroid2);
+                break;
+            case 3:
+                asteroid = Instantiate(asteroid3);
+                break;
+            case 4:
+                asteroid = Instantiate(asteroid4);
+                break;
+            default:
+                asteroid = Instantiate(asteroid1);
+                break;
+        }
+
+        asteroid.SetActive(true);
+        float xPos = Random.Range(-8.0f, 8.0f);
+
+        // Spawn asteroid just above top of screen at a random point along x-axis
+        asteroid.transform.position = new Vector3(xPos, 7.35f, 0);
+
+        asteroid.GetComponent<Asteroid>().MaxHealth();
+
+        asteroids.Add(asteroid);
+
+        return asteroid;
     }
+    
 }
