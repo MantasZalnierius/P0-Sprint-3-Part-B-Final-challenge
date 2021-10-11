f2803c2d6efb6a1dcd596262db9a7b212eabb4f5
Michael R Ryan <45245378+MichaelRRyan@users.noreply.github.com>
2021-10-11 16:19:51 +0100

    [RWMP21-88] Completed feature "Shrapnel" with tests
    * [RWMP21-88] Completed task "create shrapnel on impact" with tests
    
    * [RWMP21-89
    ] Added shrapnel spawning on asteroid collision
    
    * [RWMP21-89] Modified shrapnel spawning and added a test
    
    * [RWMP21-89] Added an additional test
    
    * [RWMP21-88] Completed task "shrapnel behaves like a bullet" with tests
    
    * [RWMP21-90] Made the shrapnel behave like a bullet
    
    * [RWMP21-90] Added a test shrapnel movement
    
    * [RWMP21-90] Added a test for shrapnel destroying asteroids
    
    * [RWMP21-90] Simplified the tests and added comments
    
    * [RWMP21-66] Reload feature implemented
    
    * [RWMP21-66] Added empty ReloadTest script
    
    * Reload delay implemented, tests passing
    
    * Fire input ignored when reloading in progress, tests passing
    
    * [RWMP21-88] Completed task "create shrapnel on impact" with tests
    
    * [RWMP21-89
    ] Added shrapnel spawning on asteroid collision
    
    * [RWMP21-89] Modified shrapnel spawning and added a test
    
    * [RWMP21-89] Added an additional test
    
    * [RWMP21-88] Completed task "shrapnel behaves like a bullet" with tests
    
    * [RWMP21-90] Made the shrapnel behave like a bullet
    
    * [RWMP21-90] Added a test shrapnel movement
    
    * [RWMP21-90] Added a test for shrapnel destroying asteroids
    
    * [RWMP21-90] Simplified the tests and added comments
    
    * Applied a potential fix
    
    * [RWMP21-88] Completed feature 'Shrapnel' with tests
    
    Co-authored-by: Ben Millar <44114751+ben-millar@users.noreply.github.com>

---
+++ Crashteroids Starter/Assets/RW/Scripts/Shrapnel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrapnel : MonoBehaviour
+{
+    public Spawner spawner;
+
+    private void Start()
+    {
+        transform.Rotate(Vector3.forward, Random.Range(0.0f, 360.0f));
+    }
+
+    void Update()
+    {
+        transform.Translate(transform.up * Time.deltaTime * 5);
+        if (transform.position.y > 10 || transform.position.y < -10
+            || transform.position.x > 10 || transform.position.x < -10)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Asteroid>() != null)
+        {
+            Game.AsteroidDestroyed();
+            Destroy(gameObject);
+            spawner.asteroids.Remove(collision.gameObject);
+            Destroy(collision.gameObject);
+        } 
+        // Stops the shrapnel from getting stuck on the player.
+        else if (collision.gameObject.GetComponent<Ship>() != null)
+        {
+            spawner.shrapnelPieces.Remove(gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Crashteroids Starter/Assets/RW/Tests/ShrapnelTest.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ShrapnelTest
+    {
+        private Game game;
+
+        [SetUp]
+        public void Setup()
+        {
+            GameObject gameGameObject =
+                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
+            game = gameGameObject.GetComponent<Game>();
+
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = asteroid.transform.position;
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Object.Destroy(game.gameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator LasersSpawnShrapnel()
+        {
+            yield return new WaitForSeconds(0.1f); // Wait for the Setup to spawn shrapnel.
+
+            // The laser and asteroid are created in Setup, we just need to check the shrapnel spawned.
+            Assert.AreEqual(4, game.GetSpawner().shrapnelPieces.Count);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator ShrapnelIsClearedOnNewGame()
+        {
+            yield return new WaitForSeconds(0.1f); // Wait for the Setup to spawn shrapnel.
+
+            // Makes sure there's at least one shrapnel before starting a new game.
+            Assert.Greater(game.GetSpawner().shrapnelPieces.Count, 1);
+            game.NewGame();
+            Assert.AreEqual(0, game.GetSpawner().shrapnelPieces.Count);
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator ShrapnelMoves()
+        {
+            yield return new WaitForSeconds(0.1f); // Wait for the Setup to spawn shrapnel.
+
+            // Makes sure there's at least one shrapnel before accessing the first in the list.
+            Assert.Greater(game.GetSpawner().shrapnelPieces.Count, 1);
+            GameObject shrapnel = game.GetSpawner().shrapnelPieces[0];
+            Vector3 position = shrapnel.transform.position;
+
+            yield return new WaitForSeconds(0.1f);
+            Assert.AreNotEqual(position, shrapnel.transform.position);
+        }
+
+        [UnityTest]
+        public IEnumerator ShrapnelDestoysAsteroids()
+        {
+            yield return new WaitForSeconds(0.1f); // Wait for the Setup to spawn shrapnel.
+
+            // Makes sure there's at least one shrapnel before accessing the first in the list.
+            Assert.Greater(game.GetSpawner().shrapnelPieces.Count, 1);
+            GameObject shrapnel = game.GetSpawner().shrapnelPieces[0];
+
+            // Spawns an asteroid and makes it collide with the shrapnel.
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            shrapnel.transform.position = asteroid.transform.position;
+
+            // Wait and then check they're both null.
+            yield return new WaitForSeconds(0.1f);
+            UnityEngine.Assertions.Assert.IsNull(asteroid);
+            UnityEngine.Assertions.Assert.IsNull(shrapnel);
+        }
+    }
+}
