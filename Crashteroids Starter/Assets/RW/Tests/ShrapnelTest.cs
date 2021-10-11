7975e74183879a8313b7bfb9f7cc229b8f1080fb
Michael R Ryan <c00239510@itcarlow.ie>
2021-10-11 15:54:24 +0100

    [RWMP21-90] Simplified the tests and added comments

--- Crashteroids Starter/Assets/RW/Tests/ShrapnelTest.cs
+++ Crashteroids Starter/Assets/RW/Tests/ShrapnelTest.cs
@@ -16,6 +16,10 @@ namespace Tests
             GameObject gameGameObject =
                 MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Game"));
             game = gameGameObject.GetComponent<Game>();
+
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = asteroid.transform.position;
         }
 
         [TearDown]
@@ -27,66 +31,46 @@ namespace Tests
         [UnityTest]
         public IEnumerator LasersSpawnShrapnel()
         {
-            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
-            GameObject laser = game.GetShip().SpawnLaser();
-            laser.transform.position = asteroid.transform.position;
-
-            yield return new WaitForSeconds(0.1f);
-
+            // The laser and asteroid are created in Setup, we just need to check the shrapnel spawned.
             Assert.AreEqual(4, game.GetSpawner().shrapnelPieces.Count);
+            yield return null;
         }
 
         [UnityTest]
         public IEnumerator ShrapnelIsClearedOnNewGame()
         {
-            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
-            GameObject laser = game.GetShip().SpawnLaser();
-            laser.transform.position = asteroid.transform.position;
-
-            yield return new WaitForSeconds(0.1f);
-
-            Assert.AreEqual(4, game.GetSpawner().shrapnelPieces.Count);
-
+            // Makes sure there's at least one shrapnel before starting a new game.
+            Assert.Greater(game.GetSpawner().shrapnelPieces.Count, 1);
             game.NewGame();
-
             Assert.AreEqual(0, game.GetSpawner().shrapnelPieces.Count);
+            yield return null;
         }
 
         [UnityTest]
         public IEnumerator ShrapnelMoves()
         {
-            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
-            GameObject laser = game.GetShip().SpawnLaser();
-            laser.transform.position = asteroid.transform.position;
-
-            yield return new WaitForSeconds(0.1f);
-
+            // Makes sure there's at least one shrapnel before accessing the first in the list.
             Assert.Greater(game.GetSpawner().shrapnelPieces.Count, 1);
             GameObject shrapnel = game.GetSpawner().shrapnelPieces[0];
             Vector3 position = shrapnel.transform.position;
 
             yield return new WaitForSeconds(0.1f);
-
             Assert.AreNotEqual(position, shrapnel.transform.position);
         }
 
         [UnityTest]
         public IEnumerator ShrapnelDestoysAsteroids()
         {
-            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
-            GameObject laser = game.GetShip().SpawnLaser();
-            laser.transform.position = asteroid.transform.position;
-
-            yield return new WaitForSeconds(0.1f);
-
+            // Makes sure there's at least one shrapnel before accessing the first in the list.
             Assert.Greater(game.GetSpawner().shrapnelPieces.Count, 1);
             GameObject shrapnel = game.GetSpawner().shrapnelPieces[0];
 
-            asteroid = game.GetSpawner().SpawnAsteroid();
+            // Spawns an asteroid and makes it collide with the shrapnel.
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
             shrapnel.transform.position = asteroid.transform.position;
 
+            // Wait and then check they're both null.
             yield return new WaitForSeconds(0.1f);
-
             UnityEngine.Assertions.Assert.IsNull(asteroid);
             UnityEngine.Assertions.Assert.IsNull(shrapnel);
         }
