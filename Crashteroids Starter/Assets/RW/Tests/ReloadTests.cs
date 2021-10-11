aac747776957fa8ffd9e90934ab20e1c6aeaeb5f
Ben Millar <44114751+ben-millar@users.noreply.github.com>
2021-10-11 16:00:19 +0100

    [RWMP21-66] Reload feature implemented
    * [RWMP21-66] Added empty ReloadTest script
    
    * Reload delay implemented, tests passing
    
    * Fire input ignored when reloading in progress, tests passing

---
+++ Crashteroids Starter/Assets/RW/Tests/ReloadTests.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class LaserTest
+{
+    private Game game;
+    private Ship ship;
+
+    [SetUp]
+    public void Setup()
+    {
+        GameObject gameGameObject =
+          MonoBehaviour.Instantiate(
+            Resources.Load<GameObject>("Prefabs/Game"));
+        game = gameGameObject.GetComponent<Game>();
+        ship = game.GetShip();
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(ship);
+        Object.Destroy(game.gameObject);
+    }
+
+    [UnityTest]
+    public IEnumerator TestReloadDelay()
+    {
+        for (int i = 0; i < 11; ++i)
+        {
+            Assert.False(ship.isReloading);
+            ship.ShootLaser();
+            yield return new WaitForSeconds(0.4f);
+        }
+
+        Assert.True(ship.isReloading);
+        yield return new WaitForSeconds(2.0f);
+        Assert.False(ship.isReloading);
+    }
+
+    [UnityTest]
+    public IEnumerator TestFireDisabledWhileReloading()
+    {
+        Assert.AreEqual(ship.GetCurrentAmmo(), 10);
+
+        for (int i = 0; i < 3; ++i)
+        {
+            ship.ShootLaser();
+            yield return new WaitForSeconds(0.4f);
+        }
+
+        Assert.AreEqual(ship.GetCurrentAmmo(), 7);
+
+        ship.isReloading = true;
+
+        for (int i = 0; i < 3; ++i)
+        {
+            ship.ShootLaser();
+            yield return new WaitForSeconds(0.4f);
+        }
+
+        Assert.AreEqual(ship.GetCurrentAmmo(), 7);
+    }
+}
